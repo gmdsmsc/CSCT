2d484af556cc5ce687b9ceb83639883e3a55a25f
gmdsmsc <182660078+gmdsmsc@users.noreply.github.com>
2026-06-19 20:16:38 +0100

    Set final model

--- openstl/models/__init__.py
+++ openstl/models/__init__.py
@@ -1,7 +1,7 @@
 #from .TSST_PredFormer import PredFormer as InteractivePredictionModel
-from .TSST_conditioning_only import InteractivePredictionModel as InteractivePredictionModel
+#from .TSST_conditioning_only import InteractivePredictionModel as InteractivePredictionModel
 #from .TSST_attention_only import InteractivePredictionModel as InteractivePredictionModel
-#from .TSST_final import InteractivePredictionModel
+from .TSST_final import InteractivePredictionModel
 
 __all__ = [
     'InteractivePredictionModel',
